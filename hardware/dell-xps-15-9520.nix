4b273716e0338362e58b63044e17de378d5963d0
Craig Wilson <craiggwilson@gmail.com>
2023-09-30 11:14:10 -0500

    udpated kernel.

--- hardware/dell-xps-15-9520.nix
+++ hardware/dell-xps-15-9520.nix
@@ -13,20 +13,25 @@
       kernelModules = [ ];
     };
     kernelModules = [ "kvm-intel" "v4l2loopback" ];
+    kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = [ ];
     extraModulePackages = with config.boot.kernelPackages;[ 
       v4l2loopback.out 
     ];
     extraModprobeConfig = ''
       options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
-      options iwlwifi power_save=1 disable_11ax=1
     '';
+  #    options iwlwifi power_save=1 disable_11ax=1
   };
+
+  
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
   environment.systemPackages = with pkgs; [ 
     libcamera
+    lshw
+    neofetch
     nvtop 
   ];
 
@@ -46,12 +51,6 @@
       enable = true;
       driSupport32Bit = true;
       driSupport = true;
-      extraPackages = with pkgs; [
-        intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-        vaapiVdpau
-        libvdpau-va-gl
-      ];
     };
   };
 
--- hardware/microsoft-surface-book-2.nix
+++ hardware/microsoft-surface-book-2.nix
@@ -36,6 +36,8 @@
 
   environment.systemPackages = with pkgs; [ 
     libcamera
+    lshw
+    neofetch
     nvtop 
   ];
 
