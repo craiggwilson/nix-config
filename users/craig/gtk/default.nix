a83f8551a7654aa6b1c5c64f37a1894992f53828
Craig Wilson <craiggwilson@gmail.com>
2023-10-09 11:01:42 -0500

    dark theme for gtk apps working.

--- users/craig/gtk/default.nix
+++ users/craig/gtk/default.nix
@@ -1,23 +1,12 @@
 { lib, pkgs, config, ... }:
 {
   hdwlinux.home.extraOptions.gtk = {
-    iconTheme = {
-      name = "elementary-Xfce-dark";
-      package = pkgs.elementary-xfce-icon-theme;
-    };
-    theme = {
-      name = "zukitre-dark";
-      package = pkgs.zuki-themes;
-    };
+    enable = true;
     gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+      gtk-application-prefer-dark-theme = 1;
     };
     gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+      gtk-application-prefer-dark-theme = 1;
     };
   };
 }
