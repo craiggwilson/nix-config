8044bda3f55c9e47d67041b6c6b3234c97424acb
Craig Wilson <craiggwilson@gmail.com>
2023-12-19 17:25:15 -0600

    added activation for adding flathub.

--- modules/nixos/features/flatpak/default.nix
+++ modules/nixos/features/flatpak/default.nix
@@ -11,5 +11,11 @@ in
 
   config = mkIf cfg.enable {
     services.flatpak.enable = true;
+
+    system.activationScripts = {
+      flathub = ''
+        /run/current-system/sw/bin/flatpak remote-add --system --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+      '';
+    };
   };
 }
