01c75b7a2f2e9b47c937997fbdf3f63294ea10df
Craig Wilson <craiggwilson@gmail.com>
2023-12-21 15:17:26 -0600

    reworked ssh into update script.

--- modules/home/features/ssh/default.nix
+++ modules/home/features/ssh/default.nix
@@ -4,32 +4,42 @@ with lib;
 with lib.hdwlinux;
 let
   cfg = config.hdwlinux.features.ssh;
+
+  knownHostsFilePath = "${config.home.homeDirectory}/.ssh/known_hosts";
+  sshConfigFilePath = "${config.home.homeDirectory}/.ssh/config";
+  sshConfigFile = pkgs.writeText "ssh_config" ''
+    # Default
+    Host *
+      ForwardAgent no
+      Compression no
+      ServerAliveInterval 0
+      ServerAliveCountMax 3
+      HashKnownHosts no
+      UserKnownHostsFile ~/.ssh/known_hosts
+      ControlMaster no
+      ControlPath ~/.ssh/master-%r@%n:%p
+      ControlPersist no
+  '';
 in
 {
   options.hdwlinux.features.ssh = with types; {
     enable = mkEnableOpt ["cli"] config.hdwlinux.features.tags;
-    includes = mkOpt (listOf str) [ ] "Other files to include in the ssh config file.";
+    includes = mkOpt (listOf path) [ ] "Other files to include in the ssh config file.";
+    knownHosts = mkOpt (listOf path) [ ] "Known hosts to include in the known hosts file.";
   };
 
   config = mkIf cfg.enable {
-    home.activation = {
-      sshConfig = inputs.home-manager.lib.hm.dag.entryAfter ["writeBoundary"] ''
-        $DRY_RUN_CMD cat << EOF > ${config.home.homeDirectory}/.ssh/config
-        ${concatStringsSep "\n" (map (i: "#Include ${i}") cfg.includes)}
+    home.packages = [
+      (pkgs.writeShellScriptBin "update-ssh" ''
+        rm -f ${sshConfigFilePath}
+        ${concatStringsSep "\n" (map (i: "cat ${i} >> ${sshConfigFilePath}") cfg.includes)}
+        cat ${sshConfigFile} >> ${sshConfigFilePath}
+        chmod 600 ${sshConfigFilePath}
 
-        Host *
-          ForwardAgent no
-          Compression no
-          ServerAliveInterval 0
-          ServerAliveCountMax 3
-          HashKnownHosts no
-          UserKnownHostsFile ~/.ssh/known_hosts
-          ControlMaster no
-          ControlPath ~/.ssh/master-%r@%n:%p
-          ControlPersist no
-        EOF
-        $DRY_RUN_CMD chmod 600 ${config.home.homeDirectory}/.ssh/config
-      '';
-    };
+        rm -f ${knownHostsFilePath}
+        ${concatStringsSep "\n" (map (i: "cat ${i} >> ${knownHostsFilePath}") cfg.knownHosts)}
+        chmod 600 ${knownHostsFilePath}
+      '')
+    ];
   };
 }
