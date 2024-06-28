184ed6a5314493c77a042fc1e2816fd53431ca7c
Craig Wilson <craiggwilson@gmail.com>
2024-06-28 15:42:02 -0500

    added sidebery and userChrome to firefox.

--- modules/home/features/discord/default.nix
+++ modules/home/features/discord/default.nix
@@ -1,15 +1,13 @@
-{ options, config, lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-with lib;
-with lib.hdwlinux;
 let cfg = config.hdwlinux.features.discord;
 in
 {
-  options.hdwlinux.features.discord = with types; {
-    enable = mkEnableOpt [ "gui" ] config.hdwlinux.features.tags;
+  options.hdwlinux.features.discord = {
+    enable = lib.hdwlinux.mkEnableOpt [ "gui" ] config.hdwlinux.features.tags;
   };
 
-  config.home.packages = with pkgs; mkIf cfg.enable [
-    discord
+  config.home.packages = lib.mkIf cfg.enable [
+    pkgs.discord
   ];
 }
--- modules/home/features/firefox/default.nix
+++ modules/home/features/firefox/default.nix
@@ -1,14 +1,12 @@
-{ options, config, lib, pkgs, ... }:
-with lib;
-with lib.hdwlinux;
+{ config, lib, pkgs, ... }:
 let cfg = config.hdwlinux.features.firefox;
 in
 {
-  options.hdwlinux.features.firefox = with types; {
-    enable = mkEnableOpt [ "gui" ] config.hdwlinux.features.tags;
+  options.hdwlinux.features.firefox = {
+    enable = lib.hdwlinux.mkEnableOpt [ "gui" ] config.hdwlinux.features.tags;
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     programs.firefox = {
       enable = true;
       profiles."default" = {
@@ -23,14 +21,58 @@ in
           "browser.toolbars.bookmarks.visibility" = "always";
           "privacy.trackingprotection.enabled" = true;
           "signon.rememberSignons" = false;
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
           "trailhead.firstrun.didSeeAboutWelcome" = true;
         };
 
+        # containers = {
+        #   "Personal" = {
+        #     id = 1;
+        #     color = "blue";
+        #     icon = "fingerprint";
+        #   };
+        #   "Work" = {
+        #     id = 2;
+        #     color = "orange";
+        #     icon = "briefcase";
+        #   };
+        #   "Banking" = {
+        #     id = 3;
+        #     color = "green";
+        #     icon = "dollar";
+        #   };
+        #   "Heather" = {
+        #     id = 4;
+        #     color = "pink";
+        #     icon = "cart";
+        #   };
+        #   "Local" = {
+        #     id = 5;
+        #     color = "red";
+        #     icon = "fence";
+        #   };
+        # };
+
+        userChrome = ''
+          #main-window #titlebar {
+            overflow: hidden;
+            transition: height 0.3s 0.3s !important;
+          }
+          /* Default state: Set initial height to enable animation */
+          #main-window #titlebar { height: 3em !important; }
+          #main-window[uidensity="touch"] #titlebar { height: 3.35em !important; }
+          #main-window[uidensity="compact"] #titlebar { height: 2.7em !important; }
+          /* Hidden state: Hide native tabs strip */
+          #main-window[titlepreface*="XXX"] #titlebar { height: 0 !important; }
+          /* Hidden state: Fix z-index of active pinned tabs */
+          #main-window[titlepreface*="XXX"] #tabbrowser-tabs { z-index: 0 !important; }
+        '';
 
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           multi-account-containers
           musescore-downloader
           onepassword-password-manager
+          sidebery
           temporary-containers
           user-agent-string-switcher
         ];
