b12ecd84b7ebad18022e5af0de77bd15f68f4559
Craig Wilson <craiggwilson@gmail.com>
2024-07-02 16:10:59 -0500

    added hyprexpo plugin.

--- modules/home/features/hyprland/default.nix
+++ modules/home/features/hyprland/default.nix
@@ -150,7 +150,7 @@ in
             "SUPER, P, exec, $colorPicker"
             "SUPER, SPACE, exec, pkill rofi || rofi -show drun -show-icons"
             "SUPER ALT , SPACE, exec, rofi -show run -show-icons"
-            "SUPER, TAB, exec, rofi -show window -show-icons"
+            "SUPER, TAB, hyprexpo:expo, toggle" #exec, rofi -show window -show-icons"
             "SUPER, T, exec, kitty"
             "SUPER, GRAVE, togglespecialworkspace, dropdown"
             "SUPER, V, exec, cliphist list | rofi -dmenu | cliphist decode | wl-copy"
@@ -235,12 +235,12 @@ in
               columns = 3;
               gap_size = 5;
               bg_col = "rgb(111111)";
-              workspace_method = "center current"; # [center/first] [workspace] e.g. first 1 or center m+1
+              workspace_method = "first 1"; # [center/first] [workspace] e.g. first 1 or center m+1
 
               enable_gesture = true; # laptop touchpad
-              gesture_fingers = 3; # 3 or 4
+              gesture_fingers = 4; # 3 or 4
               gesture_distance = 300; # how far is the "max"
-              gesture_positive = true; # positive = swipe down. Negative = swipe up.
+              gesture_positive = false; # positive = swipe down. Negative = swipe up.
             };
           };
         }
