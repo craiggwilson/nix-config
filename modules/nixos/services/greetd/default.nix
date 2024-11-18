6a18d8d47a2a383b5edec72344c337a26baa16ca
Craig Wilson <craiggwilson@gmail.com>
2024-11-18 17:15:24 -0600

    added greetd service.

---
+++ modules/nixos/services/greetd/default.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.hdwlinux.services.greetd;
+in
+{
+  options.hdwlinux.services.greetd = {
+    enable = lib.mkOption {
+      description = "greetd, a minimal and flexible login manager daemon";
+      type = lib.types.bool;
+      default = false;
+    };
+    user = lib.mkOption {
+      description = "user to login automatically";
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.greetd = {
+      enable = true;
+      settings = rec {
+        initial_session = {
+          command = "dbus-run-session Hyprland";
+          user = lib.mkIf (cfg.user != null) cfg.user;
+        };
+        default_session = initial_session;
+      };
+    };
+  };
+}
