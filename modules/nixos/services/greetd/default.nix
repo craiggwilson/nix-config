b12f80b280a4661951b8035d3a5aadb496f24e36
Craig Wilson <craiggwilson@gmail.com>
2024-12-30 18:33:50 -0600

    using tuigreet for login.

--- modules/nixos/services/greetd/default.nix
+++ modules/nixos/services/greetd/default.nix
@@ -1,15 +1,17 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 
 let
   cfg = config.hdwlinux.services.greetd;
+  cmd = "uwsm start default";
 in
 {
   options.hdwlinux.services.greetd = {
-    enable = lib.hdwlinux.mkEnableOption "greetd" false;
+    enable = lib.hdwlinux.mkEnableOption "greetd" true;
     user = lib.mkOption {
       description = "The user to login automatically.";
       type = lib.types.nullOr lib.types.str;
@@ -20,13 +22,22 @@ in
   config = lib.mkIf cfg.enable {
     services.greetd = {
       enable = true;
-      settings = rec {
-        initial_session = {
-          command = "dbus-run-session Hyprland";
-          user = lib.mkIf (cfg.user != null) cfg.user;
-        };
-        default_session = initial_session;
-      };
+      settings =
+        if cfg.user != null then
+          rec {
+            initial_session = {
+              command = cmd;
+              user = cfg.user;
+            };
+            default_session = initial_session;
+          }
+        else
+          {
+            default_session = {
+              command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --time --cmd \"${cmd}\"";
+              user = "greeter";
+            };
+          };
     };
   };
 }
