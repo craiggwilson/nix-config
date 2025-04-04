726f797222485bb5542c4c586ba0234b218c80c2
Craig Wilson <craiggwilson@gmail.com>
2025-04-04 18:35:17 -0500

    added jujutsu config for starship.

--- modules/home/programs/starship/default.nix
+++ modules/home/programs/starship/default.nix
@@ -25,7 +25,7 @@ in
       enableZshIntegration = config.hdwlinux.programs.zsh.enable;
       settings = {
         command_timeout = 5000;
-        format = "$nix_shell$shell$username$hostname$directory$git_branch$git_commit$git_state$git_metrics$git_status$kubernetes$line_break$character";
+        format = "$nix_shell$shell$username$hostname$directory\${custom.jj}$git_branch$git_commit$git_state$git_metrics$git_status$kubernetes$line_break$character";
 
         profiles = {
           transient = "$time $character";
@@ -46,12 +46,19 @@ in
         fill.symbol = " ";
 
         git_branch = {
+          disabled = true;
           style = config.hdwlinux.theme.colors.withHashtag.base04;
           symbol = "";
           format = "[$symbol$branch(:$remote_branch)]($style)";
         };
 
-        git_status.style = config.hdwlinux.theme.colors.withHashtag.base04;
+        git_commit.disabled = true;
+        git_metrics.disabled = true;
+        git_state.disabled = true;
+        git_status = {
+          disabled = true;
+          style = config.hdwlinux.theme.colors.withHashtag.base04;
+        };
 
         nix_shell = {
           symbol = "❄️ ";
@@ -76,6 +83,35 @@ in
           show_always = false;
           style_user = config.hdwlinux.theme.colors.withHashtag.base05;
           format = "[$user]($style) ";
+        };
+
+        custom.jj = {
+          ignore_timeout = true;
+          description = "The current jj status";
+          detect_folders = [ ".jj" ];
+          command = ''
+            jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
+              separate(" ",
+                change_id.shortest(4),
+                bookmarks,
+                "|",
+                concat(
+                  if(conflict, "💥"),
+                  if(divergent, "🚧"),
+                  if(hidden, "👻"),
+                  if(immutable, "🔒"),
+                ),
+                raw_escape_sequence("\x1b[2;34m") ++ if(empty, "(empty)"),
+                raw_escape_sequence("\x1b[2;34m") ++ if(description.first_line().len() == 0,
+                  "(no description set)",
+                  if(description.first_line().substr(0, 29) == description.first_line(),
+                    description.first_line(),
+                    description.first_line().substr(0, 29) ++ "…",
+                  )
+                ) ++ raw_escape_sequence("\x1b[0m"),
+              )
+            '
+          '';
         };
       } // cfg.extraSettings;
     };
