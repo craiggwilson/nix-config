441dbcd3d1cdf991a77dd5afa12fb8113064f44f
Craig Wilson <craiggwilson@gmail.com>
2026-01-23 20:08:54 -0600

    moved bambu-studio to stable after flake update.

--- hdwlinux/modules/programs/bambu-studio/default.nix
+++ hdwlinux/modules/programs/bambu-studio/default.nix
@@ -2,12 +2,11 @@
   config.substrate.modules.programs.bambu-studio = {
     tags = [
       "users:craig:personal"
-      "users:craig"
     ];
     homeManager =
       { pkgs, ... }:
       {
-        home.packages = [ pkgs.bambu-studio ];
+        home.packages = [ pkgs.stable.bambu-studio ];
       };
   };
 }
