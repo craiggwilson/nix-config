279c588207880d7ae8cd2ceb20c202d5d1b3ebc6
Craig Wilson <craiggwilson@gmail.com>
2026-04-04 11:20:36 -0500

    changed calibre over to using home-manager and installing the plugin.

--- modules/programs/calibre/default.nix
+++ modules/programs/calibre/default.nix
@@ -5,7 +5,16 @@
     homeManager =
       { pkgs, ... }:
       {
-        home.packages = [ pkgs.calibre ];
+        programs.calibre = {
+          enable = true;
+          plugins = [
+            "${pkgs.fetchzip {
+              url = "https://github.com/JimmXinu/FanFicFare/releases/download/v4.56.0/FanFicFarePlugin-v4.56.0.zip";
+              hash = "sha256-mslEit5mqKi7SxSPV5QDnjp+i5vN4fvynxoCV6ypYwM=";
+              stripRoot=false;
+            }}"
+          ];
+        };
       };
   };
 }
