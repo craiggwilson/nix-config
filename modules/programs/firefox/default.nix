f0a30622e8e1a3d43610d7625f346f80a46d8eb9
Craig Wilson <craiggwilson@gmail.com>
2026-05-02 14:45:38 -0500

    fix build warning.

--- modules/programs/firefox/default.nix
+++ modules/programs/firefox/default.nix
@@ -14,6 +14,7 @@
 
         programs.firefox = {
           enable = true;
+          configPath = "${config.home.homeDirectory}/.mozilla/firefox";
           profiles."default" = {
             id = 0;
             name = "default";
