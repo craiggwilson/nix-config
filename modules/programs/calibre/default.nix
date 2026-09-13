488bf707fabaf46e9f4aa4e4394b616a54b3c8db
Craig Wilson <craiggwilson@gmail.com>
2026-09-12 21:05:19 -0500

    removed calibre plugin.

--- modules/programs/calibre/default.nix
+++ modules/programs/calibre/default.nix
@@ -1,31 +1,14 @@
 {
   config.substrate.modules.programs.calibre = {
-    tags = [ "gui" "users:craig:personal" ];
+    tags = [
+      "gui"
+      "users:craig:personal"
+    ];
 
-    homeManager =
-      { pkgs, ... }:
-      let
-        fanficfareSrc = pkgs.fetchFromGitHub {
-          owner = "JimmXinu";
-          repo = "FanFicFare";
-          rev = "86832ac463d00ac6f1dfc10c94c47c0127c2a67c";
-          hash = "sha256-UJx8cpVR+yev6xWecAMCPCn9LtExiY4wtom0QHluBhI=";
-        };
-
-        fanficfarePlugin = pkgs.runCommand "fanficfare-plugin" {
-          nativeBuildInputs = [ pkgs.python3 pkgs.python3Packages.six pkgs.unzip ];
-        } ''
-          cp -r ${fanficfareSrc}/* .
-          python makeplugin.py
-          mkdir -p $out
-          unzip FanFicFare.zip -d $out
-        '';
-      in
-      {
-        programs.calibre = {
-          enable = true;
-          plugins = [ "${fanficfarePlugin}" ];
-        };
+    homeManager = {
+      programs.calibre = {
+        enable = true;
       };
+    };
   };
 }
