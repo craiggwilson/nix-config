3a9f0bed1f8c4048110ad7e37acdca19360b9fb8
Craig Wilson <craiggwilson@gmail.com>
2026-09-13 14:18:00 -0500

    added explicit hostname into ssh config.

--- modules/locations/raeford/ssh/default.nix
+++ modules/locations/raeford/ssh/default.nix
@@ -1,15 +1,30 @@
 {
   config.substrate.modules.locations.raeford.ssh = {
-    tags = [ "users:craig" "raeford" ];
-    homeManager = { config, ... }:
-    let
-      domain = config.hdwlinux.networking.domain;
-    in
-    {
-      hdwlinux.security.ssh.settings."*.${domain} *.tailc675f.ts.net" = {
-        ForwardX11 = true;
+    tags = [
+      "users:craig"
+      "raeford"
+    ];
+    homeManager =
+      {
+        config,
+        hasTag,
+        lib,
+        ...
+      }:
+      let
+        domain = config.hdwlinux.networking.domain;
+        # LAN machines/devices reachable as <name>.<domain>;
+        # each host gets entries for all others, never itself
+        machines = [
+          "blackflame"
+          "unsouled"
+        ];
+      in
+      {
+        hdwlinux.security.ssh.settings = lib.genAttrs (lib.filter (m: !(hasTag "host:${m}")) machines) (m: {
+          HostName = "${m}.${domain}";
+          User = config.home.username;
+        });
       };
-    };
   };
 }
-
